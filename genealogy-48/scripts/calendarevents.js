f60930d19386891751cc430a9e44b67bc49a6d94
marwandim <90573445+marwandim@users.noreply.github.com>
2021-10-31 14:56:18 +0200

    1368

---
+++ genealogy-48/scripts/calendarevents.js
@@ -0,0 +1,57 @@
+months=['','
+				January
+			','
+				February
+			','
+				March
+			','
+				April
+			','
+				May
+			','
+				June
+			','
+				July
+			','
+				August
+			','
+				September
+			','
+				October
+			','
+				November
+			','
+				December
+			'];
+weekdays=['
+		','Mon','
+		','
+		','
+		','
+		','
+		'];
+SpecialDay=1;
+oDic = {
+'view':'View',
+'changes':'Apply changes',
+'print':'Printer friendly',
+'export':'Export events displayed',
+'filter':'Filter events',
+'jump':'Jump to month'}
+AddEvent(20210101,'rawan abdel-kader Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E23rd\x3C/bdo\x3E birthday','birthday','','Demachkieh-rawanAbdel-kader-ind01318.htm','','','','');
+AddEvent(20220101,'rawan abdel-kader Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E24th\x3C/bdo\x3E birthday','birthday','','Demachkieh-rawanAbdel-kader-ind01318.htm','','','','');
+AddEvent(20211225,'mohamad abdel-kader \x3Cbdo dir=\\\x27ltr\\\x27\x3E19th\x3C/bdo\x3E birthday','birthday','','mohamadAbdel-kader-ind01319.htm','','','','');
+AddEvent(20210201,'ibrahim abdel-kader Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E16th\x3C/bdo\x3E birthday','birthday','','Demachkieh-ibrahimAbdel-kader-ind01320.htm','','','','');
+AddEvent(20220201,'ibrahim abdel-kader Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E17th\x3C/bdo\x3E birthday','birthday','','Demachkieh-ibrahimAbdel-kader-ind01320.htm','','','','');
+AddEvent(20211122,'lina abdel-kader Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E14th\x3C/bdo\x3E birthday','birthday','','Demachkieh-linaAbdel-kader-ind01321.htm','','','','');
+AddEvent(20210115,'ambara zakaria \x3Cbdo dir=\\\x27ltr\\\x27\x3E60th\x3C/bdo\x3E birthday','birthday','','ambaraZakaria-ind01322.htm','','','','');
+AddEvent(20220115,'ambara zakaria \x3Cbdo dir=\\\x27ltr\\\x27\x3E61st\x3C/bdo\x3E birthday','birthday','','ambaraZakaria-ind01322.htm','','','','');
+AddEvent(20210217,'cyrus nadim Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E6th\x3C/bdo\x3E birthday','birthday','','Demachkieh-cyrusNadim-ind01434.htm','','','','');
+AddEvent(20220217,'cyrus nadim Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E7th\x3C/bdo\x3E birthday','birthday','','Demachkieh-cyrusNadim-ind01434.htm','','','','');
+AddEvent(20210911,'raef nadim Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E5th\x3C/bdo\x3E birthday','birthday','','Demachkieh-raefNadim-ind01435.htm','','','','');
+AddEvent(20220911,'raef nadim Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E6th\x3C/bdo\x3E birthday','birthday','','Demachkieh-raefNadim-ind01435.htm','','','','');
+AddEvent(20210428,'luca nadim Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E2nd\x3C/bdo\x3E birthday','birthday','','Demachkieh-lucaNadim-ind01436.htm','','','','');
+AddEvent(20220428,'luca nadim Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E3rd\x3C/bdo\x3E birthday','birthday','','Demachkieh-lucaNadim-ind01436.htm','','','','');
+AddEvent(20210110,'khaled wa;id Demachkieh born','birthday','','Demachkieh-khaledWaId-ind01439.htm','','','','');
+AddEvent(20220110,'khaled wa;id Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E1st\x3C/bdo\x3E birthday','birthday','','Demachkieh-khaledWaId-ind01439.htm','','','','');
+
