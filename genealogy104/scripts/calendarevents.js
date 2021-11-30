4636e8c48d5b5b5804c283d99786d880831832ca
marwandim <90573445+marwandim@users.noreply.github.com>
2021-11-30 09:11:12 -0800

    104

---
+++ genealogy104/scripts/calendarevents.js
@@ -0,0 +1,17 @@
+months=['','January','February','March','April','May','June','July','August','September','October','November','December'];
+weekdays=['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+SpecialDay=1;
+oDic = {
+'view':'View',
+'changes':'Apply changes',
+'print':'Printer friendly',
+'export':'Export events displayed',
+'filter':'Filter events',
+'jump':'Jump to month'}
+AddEvent(20211113,'mounir najib Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E15th\x3C/bdo\x3E birthday','birthday','','Demachkieh-mounirNajib-ind01220.htm','','','','');
+AddEvent(20221113,'mounir najib Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E16th\x3C/bdo\x3E birthday','birthday','','Demachkieh-mounirNajib-ind01220.htm','','','','');
+AddEvent(20210428,'mona najib Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E12th\x3C/bdo\x3E birthday','birthday','','Demachkieh-monaNajib-ind01221.htm','','','','');
+AddEvent(20220428,'mona najib Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E13th\x3C/bdo\x3E birthday','birthday','','Demachkieh-monaNajib-ind01221.htm','','','','');
+AddEvent(20210622,'Mohamad Najib Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E9th\x3C/bdo\x3E birthday','birthday','','Demachkieh-MohamadNajib-ind01278.htm','','','','');
+AddEvent(20220622,'Mohamad Najib Demachkieh \x3Cbdo dir=\\\x27ltr\\\x27\x3E10th\x3C/bdo\x3E birthday','birthday','','Demachkieh-MohamadNajib-ind01278.htm','','','','');
+
---
+++ genealogy104/scripts/gmap_places.js
@@ -0,0 +1,4 @@
+gMapData = {"markers": [
+]
+}
+
